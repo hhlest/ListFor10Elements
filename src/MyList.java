d334dcd225c1c5a16d89cae0da0876b362d9f37a
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-03 23:34:01 +0200

    Added alternative solution (method remove())

--- src/MyList.java
+++ src/MyList.java
@@ -24,9 +24,12 @@ public class MyList<T> {
         for (int i = 0; i < 10; i++) {
             myList.add(i, e);
         }
+//        myList.remove(0);
+//        myList.remove(0);
+//        myList.remove(myList.size()-1);
 
         List<T> result = new ArrayList<>();
-        for (int i = 2; i < 9; i++) {
+        for (int i = 2; i < myList.size()-1; i++) {
             result.add(myList.get(i));
         }
         myList = result;
